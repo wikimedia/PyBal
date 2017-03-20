bb534625e65d0869e986e45d853bfb1484952bdf
Emanuele Rocca <ema@wikimedia.org>
2017-03-20 13:13:02 +0100

    Add unit tests for DNSQueryMonitoringProtocol
    Cherry-picked from 1.13
    
    Change-Id: I55a416030c6c7dc8dd4a1c0c9c75d4177520a18a

--- pybal/test/test_monitors.py
+++ pybal/test/test_monitors.py
@@ -6,10 +6,15 @@
   This module contains tests for `pybal.monitors`.
 
 """
-import unittest
 
 import pybal.util
 from pybal.monitors.idleconnection import IdleConnectionMonitoringProtocol
+from pybal.monitors.dnsquery import DNSQueryMonitoringProtocol
+
+from twisted.internet import defer
+from twisted.internet.reactor import getDelayedCalls
+from twisted.names.common import ResolverBase
+from twisted.names import dns, error
 
 from .fixtures import PyBalTestCase
 
@@ -60,3 +65,126 @@ class IdleConnectionMonitoringProtocolTestCase(PyBalTestCase):
         self.monitor.up = False
         self.monitor.buildProtocol(None)
         self.assertTrue(self.monitor.up)
+
+
+class FakeResolverOK(ResolverBase):
+    def _lookup(self, name, cls, qtype, timeout):
+        if qtype == dns.A:
+            payload = dns.Record_A(address='127.0.0.1', ttl=60)
+        else:
+            payload = dns.Record_AAAA(address='1::2', ttl=60)
+
+        rr = dns.RRHeader(name=name, type=qtype, cls=cls, ttl=60,
+                          payload=payload)
+
+        results = [rr]
+        authority = []
+        additional = []
+        return defer.succeed((results, authority, additional))
+
+
+class FakeResolverTimeoutError(ResolverBase):
+    def _lookup(self, name, cls, qtype, timeout):
+        return defer.fail(error.DNSQueryTimeoutError([]))
+
+
+class FakeResolverServerError(ResolverBase):
+    def _lookup(self, name, cls, qtype, timeout):
+        return defer.fail(error.DNSServerError([]))
+
+
+class FakeResolverNameError(ResolverBase):
+    def _lookup(self, name, cls, qtype, timeout):
+        return defer.fail(error.DNSNameError([]))
+
+
+class FakeResolverQueryRefusedError(ResolverBase):
+    def _lookup(self, name, cls, qtype, timeout):
+        return defer.fail(error.DNSQueryRefusedError([]))
+
+
+class FakeResolverUnknownError(ResolverBase):
+    def _lookup(self, name, cls, qtype, timeout):
+        return defer.fail(error.DNSUnknownError([]))
+
+
+class DNSQueryMonitoringProtocolTestCase(PyBalTestCase):
+    """Test case for `pybal.monitors.DNSQueryMonitoringProtocol`."""
+
+    def setUp(self):
+        super(DNSQueryMonitoringProtocolTestCase, self).setUp()
+        self.config = pybal.util.ConfigDict()
+        self.config['dnsquery.hostnames'] = '["en.wikipedia.org"]'
+        self.monitor = DNSQueryMonitoringProtocol(
+            self.coordinator, self.server, self.config)
+
+    def tearDown(self):
+        # There doesn't seem to be any sane way to avoid the delayed call to
+        # maybeParseConfig: https://twistedmatrix.com/trac/ticket/3745
+        for call in getDelayedCalls():
+            if call.func.func_name == 'maybeParseConfig':
+                call.cancel()
+
+    def testInit(self):
+        """Test `DNSQueryMonitoringProtocol.__init__`."""
+        monitor = DNSQueryMonitoringProtocol(None, None, self.config)
+
+        self.assertEquals(monitor.intvCheck, monitor.INTV_CHECK)
+        self.assertEquals(monitor.toQuery, monitor.TIMEOUT_QUERY)
+        self.assertEquals(monitor.hostnames, ['en.wikipedia.org'])
+        self.assertFalse(monitor.failOnNXDOMAIN)
+
+    def testRun(self):
+        """Test `DNSQueryMonitoringProtocol.run`."""
+        self.assertIsNone(self.monitor.resolver)
+        self.monitor.run()
+        self.assert_(len(self.monitor.resolver.resolvers) > 0)
+        self.monitor.stop()
+
+    def __testQuery(self, expectSuccess, fakeResolver):
+        """Install a mocked resolver to test different lookup results"""
+        self.monitor.resolver = fakeResolver()
+        query = self.monitor.check()
+
+        def test_dnsquery(results):
+            if expectSuccess:
+                self.assertTrue(self.monitor.up)
+            else:
+                self.assertFalse(self.monitor.up)
+
+        # Check test outcome upon deferred query completion
+        query.addCallback(test_dnsquery)
+        return query
+
+    def testQuerySuccessful(self):
+        self.__testQuery(expectSuccess=True, fakeResolver=FakeResolverOK)
+
+    def testQueryFailedTimeoutError(self):
+        self.__testQuery(expectSuccess=False,
+                         fakeResolver=FakeResolverTimeoutError)
+
+    def testQueryFailedServerError(self):
+        self.__testQuery(expectSuccess=False,
+                         fakeResolver=FakeResolverServerError)
+
+    def testQueryFailedNameErrorOK(self):
+        # dnsquery.fail-on-nxdomain is set to false by default. Expect success.
+        self.__testQuery(expectSuccess=True,
+                         fakeResolver=FakeResolverNameError)
+
+    def testQueryFailedNameErrorKO(self):
+        # Set dnsquery.fail-on-nxdomain to true and expect failure.
+        self.config['dnsquery.fail-on-nxdomain'] = 'true'
+        self.monitor = DNSQueryMonitoringProtocol(
+                self.coordinator, self.server, self.config)
+
+        self.__testQuery(expectSuccess=False,
+                         fakeResolver=FakeResolverNameError)
+
+    def testQueryFailedQueryRefusedError(self):
+        self.__testQuery(expectSuccess=False,
+                         fakeResolver=FakeResolverQueryRefusedError)
+
+    def testQueryFailedUnknownError(self):
+        self.__testQuery(expectSuccess=False,
+                         fakeResolver=FakeResolverUnknownError)
