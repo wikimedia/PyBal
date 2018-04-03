d2535baff3aae405232959db359cbcf46450a8e6
Valentin Gutierrez <vgutierrez@wikimedia.org>
2018-04-03 11:22:03 +0200

    Bump to version 1.15
    Change-Id: Ic87c6058dace922d84c7a3b574f526f74edd5d05

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name='PyBal',
-    version='1.14',
+    version='1.15',
     license='GPLv2+',
     author='Mark Bergsma',
     author_email='mark@wikimedia.org',
