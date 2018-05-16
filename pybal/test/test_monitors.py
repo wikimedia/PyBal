5ccb6ee5a5e26ccade20d0ba3c29404c8fccdfc9
Mark Bergsma <mark@wikimedia.org>
2018-05-16 11:42:22 +0200

    Add full unit test coverage of IdleConnection
    Change-Id: I7d9ab3f18205afe78301d38eba2bfad46e7e152f

--- pybal/test/test_monitors.py
+++ pybal/test/test_monitors.py
@@ -23,7 +23,10 @@ from twisted.python.runtime import seconds
 from twisted.python import failure
 import twisted.test.proto_helpers
 import twisted.internet.error
+import twisted.internet.tcp
 import twisted.web.client
+
+import socket
 
 import unittest, mock
 
@@ -351,32 +354,135 @@ class IdleConnectionMonitoringProtocolTestCase(BaseMonitoringProtocolTestCase):
     def testInit(self):
         """Test `IdleConnectionMonitoringProtocol.__init__`."""
         monitor = IdleConnectionMonitoringProtocol(None, self.server, self.config)
-        self.assertEquals(
-            monitor.maxDelay, IdleConnectionMonitoringProtocol.MAX_DELAY)
-        self.assertEquals(
-            monitor.toCleanReconnect,
-            IdleConnectionMonitoringProtocol.TIMEOUT_CLEAN_RECONNECT
-        )
-        self.config['idleconnection.max-delay'] = '123'
-        self.config['idleconnection.timeout-clean-reconnect'] = '456'
+        IC = IdleConnectionMonitoringProtocol
+        self.assertEqual(monitor.maxDelay, IC.MAX_DELAY)
+        self.assertEqual(monitor.toCleanReconnect, IC.TIMEOUT_CLEAN_RECONNECT)
+        self.assertEqual(monitor.keepAlive, IC.KEEPALIVE)
+        self.assertEqual(monitor.keepAliveRetries, IC.KEEPALIVE_RETRIES)
+        self.assertEqual(monitor.keepAliveIdle, IC.KEEPALIVE_IDLE)
+        self.assertEqual(monitor.keepAliveInterval, IC.KEEPALIVE_INTERVAL)
+
+        self.config.update({
+            'idleconnection.max-delay': '123',
+            'idleconnection.timeout-clean-reconnect': '456',
+            'idleconnection.keepalive': 'true',
+            'idleconnection.keepalive-retries': 5,
+            'idleconnection.keepalive-idle': 8,
+            'idleconnection.keepalive-interval': 60
+        })
         monitor = IdleConnectionMonitoringProtocol(None, self.server, self.config)
-        self.assertEquals(monitor.maxDelay, 123)
-        self.assertEquals(monitor.toCleanReconnect, 456)
+        self.assertEqual(monitor.maxDelay, 123)
+        self.assertEqual(monitor.toCleanReconnect, 456)
+        self.assertTrue(monitor.keepAlive)
+        self.assertEqual(monitor.keepAliveRetries, 5)
+        self.assertEqual(monitor.keepAliveIdle, 8)
+        self.assertEqual(monitor.keepAliveInterval, 60)
 
     def testRun(self):
         """Test `IdleConnectionMonitoringProtocol.run`."""
-        self.monitor.run()
+        super(IdleConnectionMonitoringProtocolTestCase, self).testRun()
         connector = self.reactor.connectors.pop()
         destination = connector.getDestination()
         self.assert_((destination.host, destination.port) == (self.server.host, self.server.port)
                      or (destination.host, destination.port) == (self.server.ip, self.server.port))
 
+    def testStop(self):
+        with mock.patch.object(self.monitor, 'stopTrying') as mock_stopTrying:
+            super(IdleConnectionMonitoringProtocolTestCase, self).testStop()
+        mock_stopTrying.assert_called()
+
+    def testStartedConnecting(self):
+        testConnector = mock.Mock(spec=twisted.internet.tcp.Connector)
+        testConnector.transport = mock.sentinel.transport
+        self.monitor.startedConnecting(testConnector)
+        self.assertIs(self.monitor.transport, mock.sentinel.transport)
+
+    def testClientConnectionFailed(self):
+        self.monitor.active = True
+        self.monitor.up = True
+        testConnector = mock.Mock(spec=twisted.internet.tcp.Connector)
+        testFailure = failure.Failure(
+            twisted.internet.error.ConnectError("Testing a connect error"))
+        with mock.patch.object(self.monitor, 'retry') as mock_retry:
+            self.monitor.clientConnectionFailed(testConnector, testFailure)
+        self.assertFalse(self.monitor.up)
+        mock_retry.assert_called_once_with(testConnector)
+
+    def testClientConnectionFailedNotActive(self):
+        self.monitor.active = False
+        testConnector = mock.Mock(spec=twisted.internet.tcp.Connector)
+        testFailure = failure.Failure(
+            twisted.internet.error.ConnectError("Testing a connect error"))
+        with mock.patch.object(self.monitor, '_resultDown') as mock_resultDown:
+            self.monitor.clientConnectionFailed(testConnector, testFailure)
+        mock_resultDown.assert_not_called()
+
+    def testClientConnectionLost(self):
+        self.monitor.active = True
+        self.monitor.up = True
+        testConnector = mock.Mock(spec=twisted.internet.tcp.Connector)
+        testFailure = failure.Failure(
+            twisted.internet.error.ConnectionLost("Testing lost connection"))
+        with mock.patch.object(self.monitor, 'retry') as mock_retry:
+            self.monitor.clientConnectionLost(testConnector, testFailure)
+        self.assertFalse(self.monitor.up)
+        mock_retry.assert_called_once_with(testConnector)
+
+    def testClientConnectionLostNotActive(self):
+        self.monitor.active = False
+        testConnector = mock.Mock(spec=twisted.internet.tcp.Connector)
+        testFailure = failure.Failure(
+            twisted.internet.error.ConnectionLost("Testing lost connection"))
+        with mock.patch.object(self.monitor, '_resultDown') as mock_resultDown:
+            self.monitor.clientConnectionLost(testConnector, testFailure)
+        mock_resultDown.assert_not_called()
+
+    def testClientConnectionLostCleanly(self):
+        self.monitor.active = True
+        self.monitor.up = True
+        testConnector = mock.Mock(spec=twisted.internet.tcp.Connector)
+        testFailure = failure.Failure(
+            twisted.internet.error.ConnectionDone("Testing cleanly closed connection"))
+        with mock.patch.object(self.monitor, '_connect') as mock_connect:
+            self.monitor.clientConnectionLost(testConnector, testFailure)
+        self.assertTrue(self.monitor.up)
+        mock_connect.assert_called_once_with(timeout=self.monitor.toCleanReconnect)
+
     def testClientConnectionMade(self):
         """Test `IdleConnectionMonitoringProtocol.clientConnectionMade`."""
         self.monitor.run()
         self.monitor.up = False
-        self.monitor.clientConnectionMade()
+        with mock.patch.object(self.monitor, 'resetDelay') as mock_resetDelay:
+            self.monitor.clientConnectionMade()
         self.assertTrue(self.monitor.up)
+        mock_resetDelay.assert_called_once()
+
+    def testClientConnectionMadeKeepalive(self):
+        self.monitor.transport = mock.Mock(spec=twisted.internet.tcp.Connection)
+        self.monitor.active = True
+        self.monitor.keepAlive = True
+        self.monitor.clientConnectionMade()
+        testSocket = self.monitor.transport.getHandle()
+        testSocket.setsockopt.assert_called()
+        setsockopt_args = {args[0] for args in testSocket.setsockopt.call_args_list}
+        expected_args = {
+            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        }
+        try:
+            expected_args.update({
+                (socket.SOL_TCP, socket.TCP_KEEPIDLE, self.monitor.keepAliveIdle),
+                (socket.SOL_TCP, socket.TCP_KEEPCNT, self.monitor.keepAliveRetries),
+                (socket.SOL_TCP, socket.TCP_KEEPINTVL, self.monitor.keepAliveInterval)
+            })
+        except AttributeError:
+            pass    # Not Linux
+        self.assertEqual(setsockopt_args, expected_args)
+
+    def testclientConnectionMadeKeepaliveNotActive(self):
+        self.monitor.active = False
+        with mock.patch.object(self.monitor, '_resultUp') as mock_resultUp:
+            self.monitor.clientConnectionMade()
+        mock_resultUp.assert_not_called()
 
     def testBuildProtocol(self):
         """Test `IdleConnectionMonitoringProtocol.buildProtocol`."""
@@ -384,6 +490,15 @@ class IdleConnectionMonitoringProtocolTestCase(BaseMonitoringProtocolTestCase):
         self.monitor.up = False
         self.monitor.buildProtocol(None)
         self.assertTrue(self.monitor.up)
+
+    def testConnect(self):
+        with mock.patch.object(self.monitor, 'reactor') as mock_reactor:
+            self.monitor._connect(mock.sentinel.arg1)
+        mock_reactor.connectTCP.assert_called_with(
+            self.monitor.server.ip,
+            self.monitor.server.port,
+            self.monitor,
+            mock.sentinel.arg1)
 
 
 class FakeResolverOK(ResolverBase):
