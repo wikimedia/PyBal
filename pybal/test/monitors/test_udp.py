48cad969f38194939b402fb0135ed662752d11ad
Mark Bergsma <mark@wikimedia.org>
2018-05-25 16:45:25 +0200

    Use MemoryReactorClock for testing the UDP monitor
    MemoryReactorClock interestingly has listenUNIX but not listenUDP,
    so we mock it.
    
    Change-Id: Iadcb639c4c508de8af6643f902ef25bb055ad9ca

--- pybal/test/monitors/test_udp.py
+++ pybal/test/monitors/test_udp.py
@@ -6,6 +6,12 @@
   This module contains tests for `pybal.monitors.udp`.
 """
 
+# Python imports
+import mock
+
+# Twisted imports
+import twisted.test.proto_helpers
+
 # Testing imports
 from .. import test_monitor
 
@@ -14,15 +20,18 @@ import pybal.monitor
 import pybal.util
 from pybal.monitors.udp import UDPMonitoringProtocol
 
-# Twisted imports
-import twisted.internet.reactor
 
 class UDPMonitoringProtocolTestCase(test_monitor.BaseMonitoringProtocolTestCase):
     monitorClass = UDPMonitoringProtocol
 
     def setUp(self):
         super(UDPMonitoringProtocolTestCase, self).setUp()
-        self.monitor.reactor = twisted.internet.reactor
+
+        def startProtocol(*args, **kwargs):
+            self.monitor.transport = mock.Mock()
+            self.monitor.startProtocol()
+
+        self.reactor.listenUDP = mock.Mock(side_effect=startProtocol)
 
     def testInit(self):
         self.assertEquals(self.monitor.interval,
@@ -43,6 +52,7 @@ class UDPMonitoringProtocolTestCase(test_monitor.BaseMonitoringProtocolTestCase)
         self.monitor.run()
         self.assertEquals(self.monitor.loop.running, True)
         self.assertTrue(self.monitor.up)
+        self.reactor.listenUDP.assert_called_with(0, self.monitor)
 
     def testConnectionRefused(self):
         monitor = UDPMonitoringProtocol(
