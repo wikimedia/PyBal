7638f26c4d5d87703001dc868de038ed637c2de6
Mark Bergsma <mark@wikimedia.org>
2018-07-27 17:08:55 +0200

    Do not import the test sub package from the main pybal package
    This was apparently added in cfc2cdba074267a77a5b3c87fc9dc59a0c1898be
    for Travis and Coveralls, but for currently unclear reasons.
    It loads unit test code in production running, which could have
    unintended side effects.
    
    Change-Id: I3f115c339827198ee47eeea10c1acec3a14d0063

--- pybal/__init__.py
+++ pybal/__init__.py
@@ -4,10 +4,8 @@ Copyright (C) 2006 by Mark Bergsma <mark@nedworks.org>
 
 The pybal package contains all PyBal modules
 """
-import test
 
 from .version import *
-
 
 __all__ = ('ipvs', 'monitor', 'pybal', 'util', 'monitors', 'bgp',
            'config', 'instrumentation', 'USER_AGENT_STRING')
